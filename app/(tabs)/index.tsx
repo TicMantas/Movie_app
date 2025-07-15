f3395015b8fa6d8c697b9bce32ff511dc27e46f4
TicMantas <mantas.tickus1998@gmail.com>
2025-07-15 10:04:20 +0300

    create routing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-   className="flex-1 justify-center items-center"
-    >
+    <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-accent font-bold">Welcome</Text>
     </View>
   );
