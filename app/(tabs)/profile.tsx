b73d09d58bad85020dd5225f9afa9cd12940bf72
TicMantas <mantas.tickus1998@gmail.com>
2025-07-15 13:03:06 +0300

    centered diferent pages, and styled Tabs bar

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,9 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
 
-export default function Profile () {
+export default function Profile() {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile Hello</Text>
     </View>
-  )
+  );
 }
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native';
 
 export default function Search() {
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Search</Text>
     </View>
   );
