e8b2e311b94093fa894ce01aec0581d7a6d7802c
TicMantas <mantas.tickus1998@gmail.com>
2025-07-16 14:50:58 +0300

    created movies search count to get trending movies by search

---
+++ services/appwrite.ts
@@ -0,0 +1,44 @@
+import { Client, Databases, ID, Query } from 'react-native-appwrite';
+
+//track the searches made by a user
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DB_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite Endpoint
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_MOVIE_ID!); // Your Appwrite Project ID
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.equal('searchTerm', query)
+        ]);
+
+        if (result.documents.length > 0) {
+            const existingMovie = result.documents[0];
+
+            await database.updateDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                existingMovie.$id,
+                {
+                    searchCount: (existingMovie.searchCount || 0) + 1
+                }
+            );
+        } else {
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+                searchTerm: query, // fixed typo from 'searhTerm'
+                movie_id: movie.id,
+                title: movie.title,
+                count: 1, // fixed field name from 'count'
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            });
+        }
+    } catch (error) {
+       console.log(error)
+       throw new Error('Failed to update search count');
+    }
+};
