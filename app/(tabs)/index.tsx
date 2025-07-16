a3b4542f9e7a52681923d7f2990fd7978f0c32e6
TicMantas <mantas.tickus1998@gmail.com>
2025-07-16 16:02:48 +0300

    creating bracket for trending movies with a function to get them

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,121 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
-import {useRouter} from "expo-router";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
 
 export default function Index() {
   const router = useRouter();
 
- const {data: movies, error: moviesError, loading: moviesLoading} = useFetch(()=> fetchMovies({
-  query:''
- }))
+  const {
+    data: trendingMovies,
+    error: trendingMoviesError,
+    loading: trendingMoviesLoading,
+  } = useFetch(getTrendingMovies)
+
+  const {
+    data: movies,
+    error: moviesError,
+    loading: moviesLoading,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
 
   return (
     <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="absolute w-full z-0"/>
+      <Image source={images.bg} className="absolute w-full z-0" />
 
-
-      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{
-        minHeight: '100%', paddingBottom: 10
-      }}>
-        <Image source={icons.logo} className="w-16 h-10 mt-20 mb-5 mx-auto"/>
-        {moviesLoading ? (
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        <Image source={icons.logo} className="w-16 h-10 mt-20 mb-5 mx-auto" />
+        {moviesLoading || trendingMoviesLoading? (
           <ActivityIndicator
             size="large"
             color="#000ff"
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text>Error : {moviesError?.message}</Text>
+        ) : moviesError || trendingMoviesError? (
+          <Text>Error : {moviesError?.message || trendingMoviesError?.message}</Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBar
               onPress={() => router.push("/search")}
               placeholder="Search for movie"
-              value=""
-              onChangeText={() => {}}
             />
+
+            {trendingMovies && (
+              <View className="flex-1 mt-5">
+                <Text className="text-white text-lg font-bold mb-3">
+                  Trending Movies
+                </Text>
+                <FlatList
+                  className="mb-5 mt-3"
+                  data={trendingMovies}
+                  renderItem={({ item }) => (
+                    <Text className="text-white text-sm">{item.title}</Text>
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                />
+              </View>
+            )}
 
             <>
               <Text className="text-white text-lg font-bold mt-5 mb-3">
-              Latest Movies
+                Latest Movies
               </Text>
               <FlatList
-              data={movies}
-              renderItem={({ item }) => (
-                <MovieCard 
-                  id={item.id}
-                  poster_path={item.poster_path}
-                  title={item.title}
-                  vote_average={item.vote_average}
-                  release_date={item.release_date} adult={false} backdrop_path={""} genre_ids={[]} original_language={""} original_title={""} overview={""} popularity={0} video={false} vote_count={0}                />
-              )}
-              keyExtractor={(item) => item.id.toString()}
-              numColumns={3}
-              columnWrapperStyle={{
-                justifyContent: 'flex-start',
-                gap: 20,
-                paddingRight: 5,
-                marginBottom: 10
-              }}
-              className="mt-2 pb-32"
-              scrollEnabled={false}
+                data={movies}
+                renderItem={({ item }) => (
+                  <MovieCard
+                    id={item.id}
+                    poster_path={item.poster_path}
+                    title={item.title}
+                    vote_average={item.vote_average}
+                    release_date={item.release_date}
+                    adult={false}
+                    backdrop_path={""}
+                    genre_ids={[]}
+                    original_language={""}
+                    original_title={""}
+                    overview={""}
+                    popularity={0}
+                    video={false}
+                    vote_count={0}
+                  />
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
               />
             </>
           </View>
         )}
-       
       </ScrollView>
     </View>
   );
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Databases, ID, Query } from 'react-native-appwrite';
+import { Client, Databases, ID, Query } from "react-native-appwrite";
 
 //track the searches made by a user
 
@@ -6,39 +6,52 @@ const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DB_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite Endpoint
-    .setProject(process.env.EXPO_PUBLIC_APPWRITE_MOVIE_ID!); // Your Appwrite Project ID
+  .setEndpoint("https://cloud.appwrite.io/v1") // Appwrite Endpoint
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_MOVIE_ID!); // Appwrite Project ID
 
 const database = new Databases(client);
-
+// to count the search queries made by a user
 export const updateSearchCount = async (query: string, movie: Movie) => {
-    try {
-        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-            Query.equal('searchTerm', query)
-        ]);
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", query),
+    ]);
 
-        if (result.documents.length > 0) {
-            const existingMovie = result.documents[0];
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
 
-            await database.updateDocument(
-                DATABASE_ID,
-                COLLECTION_ID,
-                existingMovie.$id,
-                {
-                    searchCount: (existingMovie.searchCount || 0) + 1
-                }
-            );
-        } else {
-            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
-                searchTerm: query, // fixed typo from 'searhTerm'
-                movie_id: movie.id,
-                title: movie.title,
-                count: 1, // fixed field name from 'count'
-                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-            });
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          searchCount: (existingMovie.searchCount || 0) + 1,
         }
-    } catch (error) {
-       console.log(error)
-       throw new Error('Failed to update search count');
+      );
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm: query, // fixed typo from 'searhTerm'
+        movie_id: movie.id,
+        title: movie.title,
+        count: 1, // fixed field name from 'count'
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
     }
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to update search count");
+  }
+};
+// to get the trending searches made by a user
+export const getTrendingMovies = async(): Promise<TrendingMovie[] | undefined> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 };
