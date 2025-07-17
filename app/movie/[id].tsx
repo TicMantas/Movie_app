c075105802ca70072835c7cf5e6f42b50976a667
TicMantas <mantas.tickus1998@gmail.com>
2025-07-17 19:38:37 +0300

    fetching and displaying movie details

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,12 +1,117 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text, ScrollView, TouchableOpacity } from "react-native";
+import React from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovieDetails } from "@/services/api";
+import { icons } from "@/constants/icons";
 
-const MovieDetails = () => {
-  return (
-    <View>
-      <Text>MovieDetails</Text>
-    </View>
-  )
+interface MovieInfoProps {
+  label: string;
+  value: string | number | null;
 }
 
-export default MovieDetails
+const MovieInfo = ({ label, value }: MovieInfoProps) => {
+  return (
+    <View className="flex-col items-start justify-center mt-5">
+      <Text className="text-light-200 font-normal text-sm">{label}</Text>
+      <Text className="text-white font-normal text-base mt-1">
+        {value || "N/A"}
+      </Text>
+    </View>
+  );
+};
+
+const MovieDetails = () => {
+  const { id } = useLocalSearchParams();
+
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(Number(id))
+  );
+  return (
+    <>
+    <View className="flex-1 bg-primary">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
+          />
+        </View>
+        <View className="flex-col items-start justify-center mt-5 px-5">
+          <Text className="text-white text-2xl font-bold">{movie?.title}</Text>
+          <View className="flex-row items-center gap-1 mt-2">
+            <Text className="text-light-200 text-sm font-bold uppercase">
+              {movie?.release_date
+                ? new Date(movie.release_date).getFullYear()
+                : "Unknown Year"}
+            </Text>
+            <Text className="text-light-200 ml-6 text-sm font-bold uppercase">
+              {movie?.runtime
+                ? `${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60}m`
+                : "Unknown Runtime"}
+            </Text>
+          </View>
+          <View className="flex-row items-center bg-dark-200 px-2 py-1 rounded-md gap-x-1 mt-2">
+            <Image source={icons.star} className="size-6" />
+            <Text className="text-light-200 text-sm font-bold uppercase">
+              {Math.round(movie?.vote_average ?? 0)}/10
+            </Text>
+            <Text className="text-light-200 text-xs font-medium">
+              ({movie?.vote_count ?? 0} votes)
+            </Text>
+          </View>
+          <MovieInfo label="Overview" value={movie?.overview ?? null} />
+          <MovieInfo
+            label="Genres"
+            value={
+              movie?.genres?.map((genre) => genre.name).join(" - ") ?? null
+            }
+          />
+          <View className="flex flex-row justify-between w-1/2 mt-5">
+            <MovieInfo
+              label="Budget"
+              value={
+                movie?.budget !== undefined && movie?.budget !== null
+                  ? `$${movie.budget / 1_000_000} million`
+                  : "N/A"
+              }
+            />
+            <MovieInfo
+              label="Revenue"
+              value={`$${Math.round(movie?.revenue ?? 0) / 1_000_000}`}
+            />
+          </View>
+          <MovieInfo
+            label="Production Companies"
+            value={
+              movie?.production_companies.map((c) => c.name).join(" - ") ?? null
+            }
+          />
+          <MovieInfo label="Status" value={movie?.status ?? null} />
+          <MovieInfo
+            label="Language"
+            value={movie?.original_language?.toUpperCase() ?? null}
+          />
+        </View>
+      </ScrollView>
+    </View>
+      <TouchableOpacity
+        className="absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center z-0"
+        onPress={router.back}
+      >
+        <Image
+          source={icons.arrow}
+          className="size-6 mr-2 mt-0.5 rotate-180"
+          tintColor="#fff"
+        />
+        <Text className="text-white font-bold">Go back</Text>
+      </TouchableOpacity>
+    </>
+    
+  );
+};
+
+export default MovieDetails;
--- services/api.ts
+++ services/api.ts
@@ -1,29 +1,52 @@
 export const TMDB_CONFIG = {
-    BASE_URL: 'https://api.themoviedb.org/3',
-    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
-    },
-}
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
 
-export const fetchMovies = async ({query} : {query: string}) => {
-    const endpoint = query
-        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` // If a query is provided, use the search endpoint
-        : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;// If no query is provided, use the discover endpoint
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` // If a query is provided, use the search endpoint
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`; // If no query is provided, use the discover endpoint
 
-    const repsonse = await fetch(endpoint, {
-        method: 'GET',
+  const repsonse = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!repsonse.ok) {
+    throw new Error(`Failed to fetch movies: ${repsonse.statusText}`);
+  }
+
+  const data = await repsonse.json();
+  return data.results;
+};
+
+export const fetchMovieDetails = async (
+  movieId: number
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
         headers: TMDB_CONFIG.headers,
-    });
+      });
 
-    if (!repsonse.ok) {
-        throw new Error(`Failed to fetch movies: ${repsonse.statusText}`);
-    }
+      if(!response.ok) throw new Error("Failed to fetch movie details");
 
-    const data = await repsonse.json();
-    return data.results;
-}
+      const data = await response.json()
+
+      return data;
+      
+  } catch (error) {
+    console.error(`Error fetching movie details for ID ${movieId}:`, error);
+    throw error;
+  }
+};
 
 // const url = 'BASE_URL'; // Replace with the actual URL you want to fetch
 // const options = {
